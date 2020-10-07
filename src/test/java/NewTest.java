84dd2ca4c93a852f744c0384ebcf5356be05ed36
Kirill <kirillliam@gmail.com>
2020-10-07 22:48:59 +0400

    commit 1: Initial

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewTest {
+    @Test
+    public void test1() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.ozon.ru/");
+
+
+}
+}
